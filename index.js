ecbee6e3fe43acd7a7a25801f6b20292c26a2fee
SajanThapa22 <sajanthapa1888@gmail.com>
2024-08-31 19:25:43 +0545

    api for products

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const products = require("./routes/products");
 const mongoose = require("mongoose");
 
+// Use CORS middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Your frontend's origin
+    methods: "GET,POST,PUT,DELETE", // Allowed methods
+    allowedHeaders: "Content-Type,Authorization", // Allowed headers
+  })
+);
+
 mongoose
-  .connect("mongodb://localhost/myshop")
-  .then(console.log("successfully connected to mongodb"))
+  .connect("mongodb://localhost:27017/myshop")
+  .then(() => console.log("successfully connected to mongodb"))
   .catch((err) => console.log("couldn't connect to mongodb", err));
 
 app.use(express.json());
 app.use("/api/products", products);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("listening on http://localhost:3000"));
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
--- routes/products.js
+++ routes/products.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const { Product } = require("../models/product");
 
-const products = [
-  {
-    id: 1,
-    name: "ab",
-    description: "this is product",
-    price: 1234,
-    image:
-      "https://assets2.razerzone.com/images/pnx.assets/7257c4132da98a3667632a86ac9a6a65/dav2-hero780h.jpg",
-  },
-];
+// const products = [
+//   {
+//     id: 1,
+//     name: "ab",
+//     description: "this is product",
+//     price: 1234,
+//     image:
+//       "https://assets2.razerzone.com/images/pnx.assets/7257c4132da98a3667632a86ac9a6a65/dav2-hero780h.jpg",
+//   },
+// ];
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const products = await Product.find();
   res.send(products);
-  console.log(products);
 });
 
 module.exports = router;
